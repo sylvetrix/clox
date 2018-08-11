ec8c43ba1872daedc90d21231ca3200056d6e9dc
sylvetrix <ambakke@comcast.net>
2018-08-11 14:04:46 -0500

    Finished through Chapter 17.4

--- src/compiler.c
+++ src/compiler.c
@@ -13,7 +13,28 @@ typedef struct
 	bool panicMode;
 } Parser;
 
+typedef enum
+{
+	PREC_NONE,
+	PREC_ASSIGNMENT,	// -
+	PREC_OR,			// or
+	PREC_AND,			// and
+	PREC_EQUALITY,		// == !=
+	PREC_COMPARISON,	// < > <= >=
+	PREC_TERM,			// + -
+	PREC_FACTOR,		// * /
+	PREC_UNARY,			// ! - +
+	PREC_CALL,			// . () []
+	PREC_PRIMARY
+} Precedence;
+
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk()
+{
+	return compilingChunk();
+}
 
 static void errorAt(Token* token, const char* message)
 {
@@ -79,16 +100,97 @@ static void consume(TokenType type, const char* message)
 	errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte)
+{
+	writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+	emitByte(byte1);
+	emitByte(byte2);
+}
+
+static void emitReturn()
+{
+	emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value)
+{
+	int constant = addConstant(currentChunk(), value);
+	if (constant > UINT8_MAX)
+	{
+		error("Too many constants in one chunk.");
+		return 0;
+	}
+
+	return (uint_8)constant;
+}
+
+static void emitConstant(Value value)
+{
+	emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void endCompiler()
+{
+	emitReturn();
+}
+
+static void grouping()
+{
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number()
+{
+	double value = strtod(parser.previous.start, NULL);
+	emitConstant(value);
+}
+
+static void unary()
+{
+	TokenType operatorType = parser.previous.type;
+
+	// Compile the operand
+	parsePrecedence(PREC_UNARY);
+
+	// Emit the operator instruction
+	switch (operatorType)
+	{
+		case TOKEN_MINUS:
+			emitByte(OP_NEGATE);
+			break;
+		default:
+			return;
+	}
+}
+
+static void parsePrecendence(Precedence precedence)
+{
+	
+}
+
+void expression()
+{
+	parsePrecedence(PREC_ASSIGNMENT);
+}
+
 bool compile(const char* source, Chunk* chunk)
 {
 	initScanner(source);
 
+	compilingChunk = chunk;
 	parser.hadError = false;
 	parser.panicMode = false;
 
 	advance();
 	expression();
 	consume(TOKEN_EOF, "Expect end of expression.");
+
+	endCompiler();
 
 	return !parser.hadError;
 }
