c05f38fa6f89c2aede136c5eae645f09afc6ac74
sylvetrix <ambakke@comcast.net>
2018-09-30 22:08:57 -0500

    Finished Chapter 19

--- src/value.c
+++ src/value.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "memory.h"
 #include "value.h"
@@ -42,6 +43,9 @@ void printValue(Value value)
 		case VAL_NUMBER:
 			printf("%g", AS_NUMBER(value));
 			break;
+		case VAL_OBJ:
+			printObject(value);
+			break;
 	}
 }
 
@@ -60,5 +64,11 @@ bool valuesEqual(Value a, Value b)
 			return true;
 		case VAL_NUMBER:
 			return AS_NUMBER(a) == AS_NUMBER(b);
+		case VAL_OBJ:
+		{
+			ObjString* aString = AS_STRING(a);
+			ObjString* bString = AS_STRING(b);
+			return ((aString->length == bString->length) && (memcmp(aString->chars, bString->chars, aString->length) == 0));
+		}
 	}
 }
